0368f4d35717b73ef6154a75070ee6dc557067de
Diomar Alexandrino <diomar.alexandrino@gmail.com>
2024-04-05 14:48:50 -0300

    second commit

---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,13 @@
+const ProjectsContainer = () => {
+    return <section className="projects-container">
+        <h2> Projetos</h2>
+        <p>Fiz alguns projetos que estão no github, alguns como free e alguns para fins didáticos</p>
+   
+
+    <a href="#" className="btn">
+        Ver Projetos
+    </a>
+    </section>
+};
+
+export default ProjectsContainer;
