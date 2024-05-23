ee5e94bd85fd4b278c1b2c1742ce11ee0946c204
Diomar Alexandrino <diomar.alexandrino@gmail.comgit>
2024-05-23 13:15:52 -0300

    update link redes

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,18 +3,23 @@ import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 import '../styles/components/socialNetworks.sass';
 
+
 const socialNetworks =[
-    {name: "linkedin", icon: <FaLinkedinIn />},
-    {name: "github" , icon: <FaGithub />},
-    {name: "instagram" , icon: <FaInstagram />},
+    {name: "linkedin", icon: <FaLinkedinIn />,href:"https://www.linkedin.com/in/diomaralexandrino-97a75631/" },
+    {name: "github" , icon: <FaGithub />,href:"https://github.com/DiomarAlexandrino/" } ,
+    {name: "instagram" , icon: <FaInstagram />, href:"https://www.instagram.com/diomaralexandrino/" },
 ];
+
+
 
 const SocialNetworks = () => {
     return(
         <section id="social-networks">
             {socialNetworks.map((network) => (
-                <a href="https://www.instagram.com/diomaralexandrino/" className="social-btn" id={network.name}key={network.name}>
+                <a
+                className="social-btn" id={network.name}key={network.name}href={network.href}>
                     {network.icon}
+                    
                 </a>
             ))}
         </section>
